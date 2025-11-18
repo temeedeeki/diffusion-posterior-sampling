f58f5afdbf75070d2a5f6b108fe1c0900f20c5c0
temeedeeki <tamago1123.1123@gmail.com>
2025-11-19 01:44:18 +0900

    style: update run_sampling.sh

--- scripts/run_sampling.sh
+++ scripts/run_sampling.sh
@@ -4,12 +4,13 @@ set -euC
 
 # $1: task
 # $2: gpu number
+# $3: save directory
 
 main(){
 uv run sample_condition.py \
-    --model_config=configs/model_config.yaml \
+    --model_config=configs/imagenet_model_config.yaml \
     --diffusion_config=configs/diffusion_config.yaml \
-    --task_config=configs/$1_config.yaml \
+    --task_config=$1 \
     --gpu=$2 \
     --save_dir=$3;
 }
